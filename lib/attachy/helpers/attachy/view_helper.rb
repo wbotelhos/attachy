406dfc02275f916bc555ce2bb60a8dff49c3fef0
Washington Botelho <wbotelhos@gmail.com>
2017-03-21 01:59:49 -0300

    makes options optionally

--- lib/attachy/helpers/attachy/view_helper.rb
+++ lib/attachy/helpers/attachy/view_helper.rb
@@ -8,23 +8,23 @@ module Attachy
       viewer.field
     end
 
-    def attachy_content(method, object, options)
+    def attachy_content(method, object, options = {})
       Viewer.new(method, object, options, self).content
     end
 
-    def attachy_file_field(method, object, options)
+    def attachy_file_field(method, object, options = {})
       Viewer.new(method, object, options, self).file_field
     end
 
-    def attachy_image(method, object, options)
+    def attachy_image(method, object, options = {})
       Viewer.new(method, object, options, self).image
     end
 
-    def attachy_link(method, object, options)
+    def attachy_link(method, object, options = {})
       Viewer.new(method, object, options, self).link
     end
 
-    def attachy_node(method, object, options)
+    def attachy_node(method, object, options = {})
       Viewer.new(method, object, options, self).node
     end
   end
